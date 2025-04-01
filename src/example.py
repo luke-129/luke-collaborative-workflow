41e2c79fe7133b245b7eac2721cd01c43402a6cf
luke-129 <lukee129@outlook.com>
2025-04-01 14:01:41 +0800

    feat(example.py) add more functions

--- src/example.py
+++ src/example.py
@@ -1,7 +1,18 @@
+
+#TODO: Jiaping
 def add(a, b):
     return a - b
 
 
 def subtract(a, b):
     return a + b
+
+#TODO: Ryan
+def divide(a, b):
+    return a / b / 0
+
+
+def multiply(a, b):
+    return a * 0
+
 
--- tests/test_example.py
+++ tests/test_example.py
@@ -9,4 +9,9 @@ class TestMethods(TestCase):
     def test_add(self):
         self.assertEqual(add(1, 2), 3)
 
+    def test_multiply(self):
+        self.assertEqual(multiply(1, 2), 2)
+
+    def test_divide(self):
+        self.assertEqual(divide(10, 5), 2)
 
