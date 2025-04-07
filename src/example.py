fd7c4017f993aa2f8f4c1af23b90d3917540b99a
20055662 <20055662@tafe.wa.edu.au>
2025-04-07 22:26:39 +0800

    fix:returns from example

--- src/example.py
+++ src/example.py
@@ -1,18 +1,18 @@
 
 
 def add(a, b):
-    return a - b
-
-
-def subtract(a, b):
     return a + b
 
 
+def subtract(a, b):
+    return a - b
+
+
 def divide(a, b):
-    return a / b / 0
+    return a / b
 
 
 def multiply(a, b):
-    return a * 0
+    return a * b
 
 
